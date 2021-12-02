96a4394ecd7dd2ec13ea2d1760635d2bdce94fff
Renze Nicolai <renze@rnplus.nl>
2021-12-03 00:28:39 +0100

    Include configuration, add serial command parser

--- trinamic.ino
+++ trinamic.ino
@@ -24,6 +24,15 @@ SOFTWARE.
 
 #include <SPI.h>
 
+#include "TMC6100_Register.h"
+#include "TMC6100_Fields.h"
+#include "TMC6100_Constants.h"
+
+#include "TMC4671_Register.h"
+#include "TMC4671_Fields.h"
+#include "TMC4671_Constants.h"
+#include "TMC4671_Variants.h"
+
 #define SPI_BUS_INST    VSPI
 #define PIN_SPI_SCK     18
 #define PIN_SPI_MISO    19
@@ -76,6 +85,59 @@ void setup() {
   digitalWrite(PIN_SPI_CS_DRV, HIGH);
   pinMode(PIN_SPI_CS_CTRL, OUTPUT);
   pinMode(PIN_SPI_CS_DRV, OUTPUT);
+
+  initDriver();
+
+  // Motor type &  PWM configuration
+  spi_write(SPI_DEV_CTRL, TMC4671_MOTOR_TYPE_N_POLE_PAIRS, 0x00030008);
+  spi_write(SPI_DEV_CTRL, TMC4671_PWM_POLARITIES, 0x00000000);
+  spi_write(SPI_DEV_CTRL, TMC4671_PWM_MAXCNT, 0x000003E7);
+  spi_write(SPI_DEV_CTRL, TMC4671_PWM_BBM_H_BBM_L, 0x00001919);
+  spi_write(SPI_DEV_CTRL, TMC4671_PWM_SV_CHOP, 0x00000007);
+  
+  // ADC configuration
+  spi_write(SPI_DEV_CTRL, TMC4671_ADC_I_SELECT, 0x18000100);
+  spi_write(SPI_DEV_CTRL, TMC4671_dsADC_MCFG_B_MCFG_A, 0x00100010);
+  spi_write(SPI_DEV_CTRL, TMC4671_dsADC_MCLK_A, 0x0051EB85);
+  spi_write(SPI_DEV_CTRL, TMC4671_dsADC_MCLK_B, 0x0051EB85);
+  spi_write(SPI_DEV_CTRL, TMC4671_dsADC_MDEC_B_MDEC_A, 0x00540054);
+  spi_write(SPI_DEV_CTRL, TMC4671_ADC_I0_SCALE_OFFSET, 0x010081CB);
+  spi_write(SPI_DEV_CTRL, TMC4671_ADC_I1_SCALE_OFFSET, 0x010081F9);
+
+  // Digital hall settings
+  spi_write(SPI_DEV_CTRL,  TMC4671_HALL_MODE, 0x00001001);
+  spi_write(SPI_DEV_CTRL,  TMC4671_HALL_PHI_E_PHI_M_OFFSET, 0x1F400000);
+  
+  // ABN encoder settings
+  spi_write(SPI_DEV_CTRL, TMC4671_ABN_DECODER_MODE, 0x00000000);
+  spi_write(SPI_DEV_CTRL, TMC4671_ABN_DECODER_PPR, 0x00000800);
+  spi_write(SPI_DEV_CTRL, TMC4671_ABN_DECODER_COUNT, 0x00000539);
+  spi_write(SPI_DEV_CTRL, TMC4671_ABN_DECODER_PHI_E_PHI_M_OFFSET, 0x00000000);
+  
+  // Limits
+  spi_write(SPI_DEV_CTRL, TMC4671_PID_TORQUE_FLUX_LIMITS, 2000);
+  
+  // PI settings
+  spi_write(SPI_DEV_CTRL, TMC4671_PID_TORQUE_P_TORQUE_I, (400 << 16) | 20);
+  spi_write(SPI_DEV_CTRL, TMC4671_PID_FLUX_P_FLUX_I, (50 << 16) | 10);
+  spi_write(SPI_DEV_CTRL, TMC4671_PID_VELOCITY_P_VELOCITY_I, (400 << 16) | 50);
+
+  // Init encoder (mode 0)
+  spi_write(SPI_DEV_CTRL, TMC4671_MODE_RAMP_MODE_MOTION, 0x00000008);
+  spi_write(SPI_DEV_CTRL, TMC4671_ABN_DECODER_PHI_E_PHI_M_OFFSET, 0x00000000);
+  spi_write(SPI_DEV_CTRL, TMC4671_PHI_E_SELECTION, 0x00000001);
+  spi_write(SPI_DEV_CTRL, TMC4671_PHI_E_EXT, 0x00000000);
+  spi_write(SPI_DEV_CTRL, TMC4671_UQ_UD_EXT, 0x000007D0);
+  delay(100);
+
+  spi_write(SPI_DEV_CTRL, TMC4671_ABN_DECODER_COUNT, 0x00000000);
+
+  // Feedback selection
+  spi_write(SPI_DEV_CTRL, TMC4671_PHI_E_SELECTION, 5); // 3 is encoder, 5 is hal
+  spi_write(SPI_DEV_CTRL, TMC4671_VELOCITY_SELECTION, 5);
+
+  // Stop mode
+  spi_write(SPI_DEV_CTRL, TMC4671_MODE_RAMP_MODE_MOTION, 0x00000000);
 }
 
 void readDrv(uint8_t addr) {
@@ -83,18 +145,115 @@ void readDrv(uint8_t addr) {
   Serial.println("DRV  " + String(addr, HEX) + ": " + String(value, HEX));
 }
 
+void setTorque(int16_t torque, int16_t flux) {
+  spi_write(SPI_DEV_CTRL, TMC4671_MODE_RAMP_MODE_MOTION, 1);
+  spi_write(SPI_DEV_CTRL, TMC4671_PID_TORQUE_FLUX_TARGET, (torque << 16) | flux);
+}
+
+void setVelocity(int16_t velocity) {
+  spi_write(SPI_DEV_CTRL, TMC4671_MODE_RAMP_MODE_MOTION, 2);
+  spi_write(SPI_DEV_CTRL, TMC4671_PID_VELOCITY_TARGET, velocity);
+}
+
+void setStop() {
+  spi_write(SPI_DEV_CTRL, TMC4671_MODE_RAMP_MODE_MOTION, 0x00000000);
+}
+
+void setVelocityPi(uint16_t p, uint16_t i) {
+  spi_write(SPI_DEV_CTRL, TMC4671_PID_VELOCITY_P_VELOCITY_I, (p << 16) | i);
+}
+
+void initDriver() {
+  // Initialize the driver chip
+  spi_write(SPI_DEV_DRV, TMC6100_GCONF,
+    (0 << TMC6100_DISABLE_SHIFT)      | // Enable
+    (0 << TMC6100_SINGLELINE_SHIFT)   | // Use individual L+H signals
+    (1 << TMC6100_FAULTDIRECT_SHIFT)  | // Fault output shows each protective action
+    (1 << TMC6100_CURRENT_ZERO_SHIFT)   // Disable current amplifier
+  );
+}
+
+String serialInput = "";
+
+int16_t  t = 0;
+int16_t  f = 0;
+
+int16_t  v = 0;
+uint16_t p = 600; // Velocity P
+uint16_t i = 100; // Velocity I
+
+void parseSerial() {
+  if (serialInput.length() < 1) return;
+  char c = serialInput[0];
+  serialInput.remove(0,1);
+  switch(c) {
+    case 'r':
+      initDriver();
+      break;
+    case 'v':
+      v = serialInput.toInt();
+      setVelocity(v);
+      break;
+    case 't':
+      t = serialInput.toInt();
+      setTorque(t, f);
+      break;
+    case 'f':
+      f = serialInput.toInt();
+      setTorque(t, f);
+      break;
+    case 'p':
+      p = serialInput.toInt();
+      setVelocityPi(p, i);
+      break;
+    case 'i':
+      i = serialInput.toInt();
+      setVelocityPi(p, i);
+      break;
+  }
+  Serial.println("V = "+ String(v, DEC) + ", P = " + String(p, DEC) + ", I = " + String(i, DEC));
+  serialInput = "";
+}
+
+void checkSerial() {
+  while (Serial.available()>0) {
+    char c = Serial.read();
+    if ((c >= ' ') && (c <= '~')) {
+      serialInput += c;
+    } else if ((c == '\n') || (c == '\r')) {
+      parseSerial();
+    } else {
+      serialInput = "";
+      Serial.println("#Received garbage, buffer cleared");
+    }
+  }
+}
+
 void loop() {
-  spi_write(SPI_DEV_DRV, 0, 0x00000044);
-  readDrv(0x00);
-  readDrv(0x01);
-  readDrv(0x04);
-  readDrv(0x07);
-  readDrv(0x08);
-  readDrv(0x09);
-  readDrv(0x0A);
-  
-  
-  Serial.print("CTRL ");
+  checkSerial();
+  /*Serial.println("Rotate right");
+  setTorque(200, 0);
+  delay(2000);
+  Serial.println("Stop");
+  setStop();
+  delay(2000);
+  Serial.println("Rotate left");
+  setTorque(-200, 0);
+  delay(2000);
+  Serial.println("Stop");
+  setStop();
+  delay(2000);*/
+
+  /*setVelocity(1000);
+  delay(2000);
+  setVelocity(0);
+  delay(2000);*/
+
+  // Switch to stop mode
+  /*Serial.println("Stop mode");
+  setStop();
+  delay(2000);*/
+  /*Serial.print("CTRL ");
   spi_write(SPI_DEV_CTRL, 1, 0);
   uint32_t hw_type = spi_read(SPI_DEV_CTRL, 0);
   char hw_type_str[5];
@@ -111,5 +270,5 @@ void loop() {
     Serial.print(" ");
   }
   Serial.println();
-  delay(100);
+  delay(100);*/
 }
